723163cc6124ee10131b5b1b531b7d2cabad147f
irisbokkes <126662958+irisbokkes@users.noreply.github.com>
2023-11-10 09:08:25 +0100

    add sql data wranging

--- data/silver/parquet_export/load.sql
+++ data/silver/parquet_export/load.sql
@@ -1,2 +1,4 @@
+COPY filtered_table FROM 'data/silver/parquet_export/filtered_table.parquet' (FORMAT 'parquet');
+COPY organization_grouped FROM 'data/silver/parquet_export/organization_grouped.parquet' (FORMAT 'parquet');
 COPY price_list FROM 'data/silver/parquet_export/price_list.parquet' (FORMAT 'parquet');
 COPY patient_timeline FROM 'data/silver/parquet_export/patient_timeline.parquet' (FORMAT 'parquet');
