576d54e5d326be056bc5044b41c0106c793d0495
irisbokkes <126662958+irisbokkes@users.noreply.github.com>
2023-12-19 10:21:53 +0100

    Textual changes

--- data/silver/parquet_export/schema.sql
+++ data/silver/parquet_export/schema.sql
@@ -1,10 +1,10 @@
 
 
 
-CREATE TABLE filtered_table(patient_id VARCHAR, social_security_number VARCHAR, prefix VARCHAR, first_name VARCHAR, last_name VARCHAR, birthDate TIMESTAMP_NS, code BIGINT, "system" VARCHAR, organization_id VARCHAR, organization_name VARCHAR, practitioner_name VARCHAR, practitioner_id VARCHAR, procedure_name VARCHAR, start_time VARCHAR, end_time VARCHAR, Vaccine_name VARCHAR, vaccine_code VARCHAR, vaccine_code_system VARCHAR);
-CREATE TABLE organization_grouped(organization VARCHAR, patient_count BIGINT);
-CREATE TABLE price_list(code VARCHAR, "system" VARCHAR, item_claimed VARCHAR, USD DOUBLE);
 CREATE TABLE patient_timeline(patient_id VARCHAR, social_security_number VARCHAR, prefix VARCHAR, first_name VARCHAR, last_name VARCHAR, birthDate TIMESTAMP_NS, code BIGINT, "system" VARCHAR, organization_id VARCHAR, organization_name VARCHAR, practitioner_name VARCHAR, practitioner_id VARCHAR, procedure_name VARCHAR, start_time VARCHAR, end_time VARCHAR, Vaccine_name VARCHAR, vaccine_code VARCHAR, vaccine_code_system VARCHAR);
+CREATE TABLE price_list(code VARCHAR, "system" VARCHAR, item_claimed VARCHAR, USD DOUBLE);
+CREATE TABLE organization_grouped(organization VARCHAR, patient_count BIGINT);
+CREATE TABLE filtered_table(patient_id VARCHAR, social_security_number VARCHAR, prefix VARCHAR, first_name VARCHAR, last_name VARCHAR, birthDate TIMESTAMP_NS, code BIGINT, "system" VARCHAR, organization_id VARCHAR, organization_name VARCHAR, practitioner_name VARCHAR, practitioner_id VARCHAR, procedure_name VARCHAR, start_time VARCHAR, end_time VARCHAR, Vaccine_name VARCHAR, vaccine_code VARCHAR, vaccine_code_system VARCHAR);
 
 
 
